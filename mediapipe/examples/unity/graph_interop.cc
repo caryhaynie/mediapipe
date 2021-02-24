a2d7ef9c2ae5a52829caf8412ce78b2a7e086c1c
Tim Mowrer <timm@unity3d.com>
2021-02-24 10:54:40 -0800

    Add graph interop

---
+++ mediapipe/examples/unity/graph_interop.cc
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+
+#include <vector>
+
+#include "export.h"
+#include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/port/parse_text_proto.h"
+
+#define LOG_AND_RETURN(CODE) \
+    do { \
+        const auto result = CODE; \
+        LOG(INFO) << __PRETTY_FUNCTION__ << ":\n" << result; \
+        return result; \
+    } while (0)
+
+namespace unity {
+    using namespace mediapipe;
+
+class Graph {
+public:
+    Graph(const std::string& configContents)
+    {
+        m_Config = mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(configContents);
+    }
+
+    template<typename T>
+    void SetInputSidePacket(const std::string& key, const T& value)
+    {
+        m_InputSidePackets[key] = mediapipe::MakePacket<T>(value);
+    }
+
+    mediapipe::Status Initialize()
+    {
+        LOG_AND_RETURN(m_Graph.Initialize(m_Config, m_InputSidePackets));
+    }
+
+    mediapipe::Status Start()
+    {
+        LOG_AND_RETURN(m_Graph.StartRun({}));
+    }
+
+private:
+    mediapipe::CalculatorGraph m_Graph;
+    mediapipe::CalculatorGraphConfig m_Config;
+    std::map<std::string, mediapipe::Packet> m_InputSidePackets;
+};
+
+EXPORT(Graph*) UnityMediaPipe_Graph_Construct_ConfigContents_(
+    const char* configContents, int32_t length)
+{
+    return new Graph(std::string(configContents, length));
+}
+
+EXPORT(void) UnityMediaPipe_Graph_SetStringInputSidePacket(
+    Graph* self, const char* key, int32_t keyLength,
+    const char* value, int32_t valueLength)
+{
+    self->SetInputSidePacket(std::string(key, keyLength), std::string(value, valueLength));
+}
+
+EXPORT(absl::StatusCode) UnityMediaPipe_Graph_Start(Graph* self)
+{
+    return self->Start().code();
+}
+
+}  // namespace mediapipe
